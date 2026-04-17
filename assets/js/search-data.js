5752f1da329d26724708e7ea97059ea740e0c281
shan1525 <70058996+shan1525@users.noreply.github.com>
2026-04-17 18:39:05 +0000

    Deploying to gh-pages from @ shan1525/shan1525.github.io@8a90b2d11d777b354a86b2f8bbb7522077b369dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Postdoctoral researcher in cosmology working on dark energy, scalar fields, and cosmological tensions.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
