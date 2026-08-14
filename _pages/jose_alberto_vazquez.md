9f5213ca05f20613bf07468f3ac3b12ec019e4cf
shan1525 <70058996+shan1525@users.noreply.github.com>
2026-08-14 20:29:25 +0000

    Deploying to gh-pages from @ shan1525/shan1525.github.io@d64c03fd6bb0edfc5f4159e44938f33a8fdef9c8 🚀

---
+++ _pages/jose_alberto_vazquez.md
@@ -0,0 +1,8 @@
+## José Alberto Vázquez González
+
+José Alberto Vázquez González is a researcher at the Instituto de Ciencias
+Físicas, Universidad Nacional Autónoma de México. His work focuses on
+observational cosmology, dark energy, inflation, Bayesian statistics, and
+scientific machine learning.
+
+[Personal website](https://www.fis.unam.mx/~javazquez/)
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,8 +31,8 @@ ninja.data = [{
             window.location.href = "/teaching/";
           },
         },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
+          title: "People",
+          description: "Collaborators and members of our cosmology research network",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
